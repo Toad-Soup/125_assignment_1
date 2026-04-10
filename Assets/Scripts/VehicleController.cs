66d580e3c742491a6bff12ee281532b2f054ad7e
torrey <tbspear88@gmail.com>
2026-04-09 19:23:44 -0700

    added player movement

---
+++ Assets/Scripts/VehicleController.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class VehicleController : MonoBehaviour
+{
+    public float desired_acceleration;
+    public float power;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Rigidbody>().AddRelativeForce(desired_acceleration*power, 0, 0);
+
+        float dx = (Mouse.current.position.x.value - Screen.width / 2) / 200;
+        if (Mathf.Abs(dx) > 0.01f)
+        {
+            transform.Rotate(0, dx, 0);
+        }
+    }
+
+    void onMove(InputValue action){
+        var movement = action.Get<Vector2>();
+        desired_acceleration = movement.y;
+    }
+}
